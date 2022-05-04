94ce346b3309488abd3961cfa66b8bb4f8650c5b
JeMinMoon <100738519+JeMinMoon@users.noreply.github.com>
2022-05-04 10:00:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
   author='jeminmoon',
   author_email='mjm1999@naver.com',
   url = "https://github.com/JeMinMoon/calculator",
-  download_url = "https://github.com/JeMinMoon/calculator/archive/refs/tags/v1.0.0.zip",
+  download_url = "https://github.com/JeMinMoon/calculator/archive/refs/tags/v1.0.1.zip",
   install_requires=['pytest'],
   long_description = 'oss-dec calculator python module',
   long_description_content_type = 'text/markdown',
